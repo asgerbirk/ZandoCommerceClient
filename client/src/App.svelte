8f2f3b886c4ffb6e480c962dfbbe87a1e674ef96
asgerbirk <asger.birk@gmail.com>
2023-05-30 19:14:41 +0200

    first

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,47 +1,36 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { Router, Route } from "svelte-navigator";
+  import Home from "./pages/Home.svelte";
+  import Navbar from "./components/Navbar.svelte";
+  import Register from "./pages/Register.svelte";
+  import Login from "./pages/Login.svelte";
+  import VerifyMail from "./pages/VerifyMail.svelte";
+  import Account from "./pages/Account.svelte";
+  import CreateProduct from "./pages/CreateProduct.svelte";
+  import Products from "./pages/Products.svelte";
+  import ChatBot from "./components/ChatBot.svelte";
+  import Footer from "./components/Footer.svelte";
+  import EditProductModal from "./pages/EditProductModal.svelte";
+  import Cart from "./pages/Cart.svelte";
+  import CartItem from "./pages/CartItem.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+
+
+<Router>
+  <Navbar/>
+  <div class="container mx-auto px-4">
+    <Route path="/" component={Login} />
+    <Route path="/home" component={Home} />
+    <Route path="/register" component={Register} />
+    <Route path="/verify-email" component={VerifyMail} />
+    <Route path="/account" component={Account} />
+    <Route path="/createProduct" component={CreateProduct} />
+    <Route path="/products" component={Products} />
+    <Route path="/chatBot" component={ChatBot} />
+    <Route path="/edit/:productId" component={EditProductModal}/>
+    <Route path="/cart" component={Cart}/>
+    <Route path="/cartItem" component={CartItem}/>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+  <Footer/>
+</Router>
